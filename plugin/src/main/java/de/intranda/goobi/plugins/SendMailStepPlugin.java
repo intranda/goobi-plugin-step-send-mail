2a572f950788333e588f1354a56d8ebb7f3c7687
Robert Sehr <robert.sehr@intranda.com>
2022-01-26 14:05:20 +0100

    send mails

--- plugin/src/main/java/de/intranda/goobi/plugins/SendMailStepPlugin.java
+++ plugin/src/main/java/de/intranda/goobi/plugins/SendMailStepPlugin.java
@@ -1,5 +1,9 @@
 package de.intranda.goobi.plugins;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+
 /**
  * This file is part of a plugin for Goobi - a Workflow tool for the support of mass digitization.
  *
@@ -20,6 +24,18 @@ package de.intranda.goobi.plugins;
  */
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.Properties;
+
+import javax.mail.Address;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 import org.apache.commons.configuration.SubnodeConfiguration;
 import org.goobi.beans.Step;
@@ -37,14 +53,14 @@ import net.xeoh.plugins.base.annotations.PluginImplementation;
 @PluginImplementation
 @Log4j2
 public class SendMailStepPlugin implements IStepPluginVersion2 {
-    
+
     @Getter
     private String title = "intranda_step_sendMail";
     @Getter
     private Step step;
     @Getter
     private String value;
-    @Getter 
+    @Getter
     private boolean allowTaskFinishButtons;
     private String returnPath;
 
@@ -52,17 +68,84 @@ public class SendMailStepPlugin implements IStepPluginVersion2 {
     public void initialize(Step step, String returnPath) {
         this.returnPath = returnPath;
         this.step = step;
-                
+
         // read parameters from correct block in configuration file
-        SubnodeConfiguration myconfig = ConfigPlugins.getProjectAndStepConfig(title, step);
-        value = myconfig.getString("value", "default value"); 
-        allowTaskFinishButtons = myconfig.getBoolean("allowTaskFinishButtons", false);
-        log.info("SendMail step plugin initialized");
+        SubnodeConfiguration config = ConfigPlugins.getProjectAndStepConfig(title, step);
+
+        String smtpServer = config.getString("smtpServer", null);
+        String smtpUser = config.getString("smtpUser", null);
+        String smtpPassword = config.getString("smtpPassword", null);
+        boolean smtpUseStartTls = config.getBoolean("smtpUseStartTls", false);
+        boolean smtpUseSsl = config.getBoolean("smtpUseSsl", false);
+        String smtpSenderAddress = config.getString("smtpSenderAddress", null);
+
+        List<String> recipients = Arrays.asList(config.getStringArray("receiver"));
+
+        String messageSubject =  config.getString("messageSubject", null);
+        String messageBody =  config.getString("messageBody", null);;
+
+        Properties props = new Properties();
+        if (smtpUseStartTls) {
+            props.setProperty("mail.transport.protocol", "smtp");
+            props.setProperty("mail.smtp.auth", "true");
+            props.setProperty("mail.smtp.port", "25");
+            props.setProperty("mail.smtp.host", smtpServer);
+            props.setProperty("mail.smtp.ssl.trust", "*");
+            props.setProperty("mail.smtp.starttls.enable", "true");
+            props.setProperty("mail.smtp.starttls.required", "true");
+        } else if (smtpUseSsl) {
+            props.setProperty("mail.transport.protocol", "smtp");
+            props.setProperty("mail.smtp.host", smtpServer);
+            props.setProperty("mail.smtp.auth", "true");
+            props.setProperty("mail.smtp.port", "465");
+            props.setProperty("mail.smtp.ssl.enable", "true");
+            props.setProperty("mail.smtp.ssl.trust", "*");
+        } else {
+            props.setProperty("mail.transport.protocol", "smtp");
+            props.setProperty("mail.smtp.auth", "true");
+            props.setProperty("mail.smtp.port", "25");
+            props.setProperty("mail.smtp.host", smtpServer);
+        }
+        List<Address> addresses = new ArrayList<>(recipients.size());
+        // create a mail for each user
+        try {
+            for (String receiver : recipients) {
+                Address address = new InternetAddress(receiver);
+                addresses.add(address);
+            }
+            Session session = Session.getDefaultInstance(props, null);
+            Message msg = new MimeMessage(session);
+
+            InternetAddress addressFrom = new InternetAddress(smtpSenderAddress);
+            msg.setFrom(addressFrom);
+            //        msg.setRecipient(Message.RecipientType.TO, new InternetAddress(user.getEmail()));
+            msg.setRecipients(Message.RecipientType.TO, addresses.toArray(new Address[addresses.size()]));
+
+            // create mail
+            MimeMultipart multipart = new MimeMultipart();
+
+            msg.setSubject(messageSubject);
+            MimeBodyPart messageHtmlPart = new MimeBodyPart();
+            messageHtmlPart.setText(messageBody, "utf-8");
+            messageHtmlPart.setHeader("Content-Type", "text/html; charset=\"utf-8\"");
+            multipart.addBodyPart(messageHtmlPart);
+
+            msg.setContent(multipart);
+            msg.setSentDate(new Date());
+
+            Transport transport = session.getTransport();
+            transport.connect(smtpUser, smtpPassword);
+            transport.sendMessage(msg, msg.getRecipients(Message.RecipientType.TO));
+            transport.close();
+        } catch (MessagingException e) {
+            log.error(e);
+        }
+
     }
 
     @Override
     public PluginGuiType getPluginGuiType() {
-        return PluginGuiType.FULL;
+        return PluginGuiType.NONE;
         // return PluginGuiType.PART;
         // return PluginGuiType.PART_AND_FULL;
         // return PluginGuiType.NONE;
@@ -70,7 +153,7 @@ public class SendMailStepPlugin implements IStepPluginVersion2 {
 
     @Override
     public String getPagePath() {
-        return "/uii/plugin_step_sendMail.xhtml";
+        return null;
     }
 
     @Override
@@ -87,7 +170,7 @@ public class SendMailStepPlugin implements IStepPluginVersion2 {
     public String finish() {
         return "/uii" + returnPath;
     }
-    
+
     @Override
     public int getInterfaceVersion() {
         return 0;
@@ -97,7 +180,7 @@ public class SendMailStepPlugin implements IStepPluginVersion2 {
     public HashMap<String, StepReturnValue> validate() {
         return null;
     }
-    
+
     @Override
     public boolean execute() {
         PluginReturnValue ret = run();
@@ -108,7 +191,7 @@ public class SendMailStepPlugin implements IStepPluginVersion2 {
     public PluginReturnValue run() {
         boolean successful = true;
         // your logic goes here
-        
+
         log.info("SendMail step plugin executed");
         if (!successful) {
             return PluginReturnValue.ERROR;
