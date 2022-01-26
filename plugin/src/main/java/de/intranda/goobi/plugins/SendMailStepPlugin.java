91880942936b590d045d25cacb09849da87e56f5
Robert Sehr <robert.sehr@intranda.com>
2022-01-26 11:37:51 +0100

    renamed plugin

--- plugin/src/main/java/de/intranda/goobi/plugins/SendMailStepPlugin.java
+++ plugin/src/main/java/de/intranda/goobi/plugins/SendMailStepPlugin.java
@@ -36,10 +36,10 @@ import net.xeoh.plugins.base.annotations.PluginImplementation;
 
 @PluginImplementation
 @Log4j2
-public class SampleStepPlugin implements IStepPluginVersion2 {
+public class SendMailStepPlugin implements IStepPluginVersion2 {
     
     @Getter
-    private String title = "intranda_step_sample";
+    private String title = "intranda_step_sendMail";
     @Getter
     private Step step;
     @Getter
@@ -57,7 +57,7 @@ public class SampleStepPlugin implements IStepPluginVersion2 {
         SubnodeConfiguration myconfig = ConfigPlugins.getProjectAndStepConfig(title, step);
         value = myconfig.getString("value", "default value"); 
         allowTaskFinishButtons = myconfig.getBoolean("allowTaskFinishButtons", false);
-        log.info("Sample step plugin initialized");
+        log.info("SendMail step plugin initialized");
     }
 
     @Override
@@ -70,7 +70,7 @@ public class SampleStepPlugin implements IStepPluginVersion2 {
 
     @Override
     public String getPagePath() {
-        return "/uii/plugin_step_sample.xhtml";
+        return "/uii/plugin_step_sendMail.xhtml";
     }
 
     @Override
@@ -109,7 +109,7 @@ public class SampleStepPlugin implements IStepPluginVersion2 {
         boolean successful = true;
         // your logic goes here
         
-        log.info("Sample step plugin executed");
+        log.info("SendMail step plugin executed");
         if (!successful) {
             return PluginReturnValue.ERROR;
         }
